bea8a9be2dd02fb15d06cac4e82e84ae42a092c1
Ainurrahmisln <ainurrahmiselian@students.amikom.ac.id>
2023-03-17 14:41:47 +0700

    menambahkan file calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,17 @@
+class calculator 
+{
+	static void main(string[] args)
+	{
+		
+	}
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static pengurangan(int a, int b)
+	{
+		return a - b;
+		
+	}
+}
