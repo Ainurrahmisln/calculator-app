8374842cdeb2ced7d781764abcbf2e64f69ce3e3
Ainurrahmisln <ainurrahmiselian@students.amikom.ac.id>
2023-03-17 14:54:30 +0700

    menambahkan file calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,11 @@ class calculator
 {
 	static void main(string[] args)
 	{
+		console.writeline(penambahan("hsil penambahan: (0) + (1) = {2} " a, b, penambahan (a , b));
+		console.writeline(penambahan("hsil pengurangan: (0) + (1) = {2} " a, b, pengurangan (a , b));
 		
+		console.writeline("\ntTekan sembarang key untuk keluar");
+		console.redkey();
 	}
 	static int penambahan(int a, int b)
 	{
@@ -12,6 +16,6 @@ class calculator
 	static pengurangan(int a, int b)
 	{
 		return a - b;
-		
+
 	}
 }
